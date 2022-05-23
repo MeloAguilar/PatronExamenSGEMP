d49e88b2ffa1ddc9df23e634763c28a4560423e8
Melo Aguilar <carmelo.aguilar@iesnervion.es>
2022-05-23 19:44:19 +0200

    No habia hecho las referencias al resto de proyectos hehe

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using UI.Models;
 
 namespace UI.Controllers
 {
@@ -18,15 +17,6 @@ namespace UI.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
